2d1a8ba7212f253deb90ba25b8d8f26962308f5c
TxsAdamWest <Twinadanz@gmail.com>
2016-06-11 12:18:23 -0500

    got our clock tickin'

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
 console.log("hello worldzz!")
 
-var dateEl = document.querySelector(".time")
+// Capturing DOM nodes.  Gotta catch'em all . Gotta catch'em all!
+
+var timeEl = document.querySelector(".time")
 var hexColorEl = document.querySelector(".hexColor")
 
-dateEl.innerHTML = new Date().getHours() +":"+ new Date().getMinutes() + ":"+ new Date().getSeconds()
+
+
+// Getting the time and turning it readable to humans.  (In HH:MM:SS format)
+
+function updateTime() {
+	console.log('tick')
+
+var d = new Date()
+
+var currentHours = d.getHours()
+var currentMinutes = d.getMinutes()
+var currentSeconds = d.getSeconds()
+
+// Choose either "AM" or "PM" as appropriate
+var timeOfDay = ( d.getHours() < 12 ) ? "AM" : "PM";
+
+
+// Convert an hours component of "0" to "12"
+currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+// Pad the minutes and seconds with leading zeros, if needed
+currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+
+// Concatenate the string and display it to the humons!
+timeEl.innerHTML = currentHours +":"+ currentMinutes + ":"+ currentSeconds + timeOfDay
+
+}
+
+setInterval(updateTime, 1000)
+
+// Now to update the hexcolor of our radial gradient background according to what time it is.
+
+
+
+
+
+
