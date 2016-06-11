f29139742790ca0d0159f7beecc70e3bb67a2df8
TxsAdamWest <Twinadanz@gmail.com>
2016-06-11 14:41:20 -0500

    debugging hex color translator

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var timeEl = document.querySelector(".time")
 var hexColorEl = document.querySelector(".hexColor")
 var bodyEl = document.querySelector('body')
 
+var testEl = document.querySelector('.textBox')
 
 // Getting the time and turning it readable to humans.  (In HH:MM:SS format)
 
@@ -31,28 +32,29 @@ currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
 
 // Concatenate the string and display it to the humons!
-timeEl.innerHTML = currentHours +":"+ currentMinutes + ":"+ currentSeconds + timeOfDay
+timeEl.innerHTML = currentHours + ":" + currentMinutes + ":" + currentSeconds + timeOfDay
 
 // Now to update the hexcolor of our radial gradient background according to what time it is.
 
-var hexHours = (parseInt(currentHours) - 10) + currentHours.toString(16)
+var hexHours = parseInt(currentHours) - 10 + currentHours.toString(16)
 var hexMinutes = currentMinutes.toString(16)
 var hexSeconds = currentSeconds.toString(16)
 
-hexColorEl.innerHTML= '#' + hexHours + hexMinutes + hexSeconds 
+hexColorEl.innerHTML = '#' + hexHours + hexMinutes + hexSeconds 
+
 
 var updatedHexColor = hexColorEl.innerHTML
 
-console.log(bodyEl.style.backgroundImage)
-console.log(timeEl.style.position)
+// console.log(typeof updatedHexColor)
+// console.log(updatedHexColor)
+// console.log(parseInt(hexMinutes))
+// console.log(typeof hexSeconds)
 
-// bodyEl.style.backgroundImage = "radial-gradient(circle at 50% 50% , " + updatedHexColor + "0%, #0000FF 100%);"
+testEl.style.background = 
+
 
 // var updatedHexColorJQ = this.updatedHexColor
-
-// console.log($('body').css('background-image', 'radial-gradient(circle at 50% 50%' + updatedHexColorJQ )
-// )
-// timeEl.style.backgroundImage = "radial-gradient(circle at 50% 50% , " + updatedHexColor + "0%, #0000FF 100%);"
+// console.log($('body').css('background-image', 'radial-gradient(circle at 50% 50% ' + updatedHexColorJQ + ');' )
 
 }
 
