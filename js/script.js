88f88bd28473171fdcd5e363cceab82b418a549c
TxsAdamWest <Twinadanz@gmail.com>
2016-06-12 21:45:52 -0500

    Hex changing, timee ticking, need to get line to expand

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@ console.log("hello worldzz!")
 var timeEl = document.querySelector(".time")
 var hexColorEl = document.querySelector(".hexColor")
 var bodyEl = document.querySelector('body')
-
-var testEl = document.querySelector('.textBox')
+var lineEl = document.querySelector('.line')
 
 // Getting the time and turning it readable to humans.  (In HH:MM:SS format)
 
@@ -27,6 +26,7 @@ var timeOfDay = ( d.getHours() < 12 ) ? "AM" : "PM";
 currentHours = ( currentHours == 0 ) ? 12 : currentHours;
 
 // Pad the minutes and seconds with leading zeros, if needed
+currentHours = (currentHours < 10 ? "0": "") + currentHours;
 currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
 currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
@@ -36,27 +36,26 @@ timeEl.innerHTML = currentHours + ":" + currentMinutes + ":" + currentSeconds + 
 
 // Now to update the hexcolor of our radial gradient background according to what time it is.
 
-var hexHours = parseInt(currentHours) - 10 + currentHours.toString(16)
+var hexHours =  currentHours.toString(16)
 var hexMinutes = currentMinutes.toString(16)
 var hexSeconds = currentSeconds.toString(16)
 
 hexColorEl.innerHTML = '#' + hexHours + hexMinutes + hexSeconds 
 
-
+// Hex color is now concatenated into a string readable by CSS.
 var updatedHexColor = hexColorEl.innerHTML
 
-// console.log(typeof updatedHexColor)
-// console.log(updatedHexColor)
-// console.log(parseInt(hexMinutes))
-// console.log(typeof hexSeconds)
-
-testEl.style.background = 
+// Passing in the updated hex color to our background. 
+bodyEl.style.background = 'radial-gradient(circle at 50% 50% , '+ updatedHexColor +' 0%, #FFF 100%)'
 
 
-// var updatedHexColorJQ = this.updatedHexColor
-// console.log($('body').css('background-image', 'radial-gradient(circle at 50% 50% ' + updatedHexColorJQ + ');' )
+
+lineEl.style.width += 2   
+
+
 
 }
+
 
 setInterval(updateTime, 1000)
 
